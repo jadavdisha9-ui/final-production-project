ab9e09d6344aa23b006ced255db71d357b88480d
jadavdisha9-ui <jadavdisha9@gmail.com>
2026-04-11 18:45:05 +0530

    Update version message in home route

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Auto Deploy Working - Version 2"
+    return "Auto Deploy Working - Version 3"
 
 @app.route("/health")
 def health():
